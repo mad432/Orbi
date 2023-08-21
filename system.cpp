c2fe060001e9d8f648c1628c292469c2a83ed236
mad432 <113856205+mad432@users.noreply.github.com>
2023-08-21 09:30:00 -0600

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -179,6 +179,7 @@ void System::collision(Particle* par, Particle* par1){
         double breakmass = 0;
         double break_mom_x = 0;//break away particles momentum
         double break_mom_y = 0;
+        
         if((100 * pow(rel_speed,2)) / (collision_angle+20) * (0.3 + relmass * 0.3) > *col_threshold && rel_speed > 10 && !Special_rel){//determines if any new particles will be created from collision
             //std::cout<<"new Particles"<<std::endl;
             double breaksize;
